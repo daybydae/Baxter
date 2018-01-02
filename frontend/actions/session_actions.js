76ad772d4cf93f088122b52b670fbe8f41d6fbe5
Dae Kim <dae.dkim6@gmail.com>
2018-01-02 17:09:49 -0500

    Up to RootReducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,7 +1,7 @@
 import * as SessionApiUtil from './util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const RECEIVE_ERRORS = "REMOVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 export const login = (user) => dispatch => {
   return SessionApiUtil.login(user).then( user => dispatch(receiveCurrentUser(user)), error => dispatch(receiveErrors(error)));
@@ -24,7 +24,7 @@ export const receiveCurrentUser = user => {
 
 export const receiveErrors = (error) => {
   return {
-    type: RECEIVE_ERRORS,
+    type: RECEIVE_SESSION_ERRORS,
     error
   };
 };
