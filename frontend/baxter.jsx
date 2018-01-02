41ec37a50da37b13eb71dc6c418d80f38fa62251
Dae Kim <dae.dkim6@gmail.com>
2018-01-02 17:47:55 -0500

    Up to Phase 1

--- frontend/baxter.jsx
+++ frontend/baxter.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   window.login = SessionApiUtil.login;
   window.signup = SessionApiUtil.signup;
   window.logout = SessionApiUtil.logout;
+
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Baxter.com</h1>, root);
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,3 +2,10 @@ import { combineReducers } from 'redux';
 
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
+
+const rootReducer = combineReducers({
+  sessionReducer,
+  errorsReducer
+});
+
+export default rootReducer;
