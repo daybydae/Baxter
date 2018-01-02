c1e1ef8082ff408f762925f60d533850a69a398b
Dae Kim <dae.dkim6@gmail.com>
2018-01-02 15:29:58 -0500

    Up to sessionREducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,30 @@
+import * as SessionApiUtil from './util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "REMOVE_ERRORS";
+
+export const login = (user) => dispatch => {
+  return SessionApiUtil.login(user).then( user => dispatch(receiveCurrentUser(user)), error => dispatch(receiveErrors(error)));
+};
+
+export const logout = () => dispatch => {
+  return SessionApiUtil.logout().then( () => dispatch(receiveCurrentUser(null)), error => dispatch(receiveErrors(error)));
+};
+
+export const signup = (user) => dispatch => {
+  return SessionApiUtil.signup(user).then( user => dispatch(receiveCurrentUser(user)), error => dispatch(receiveErrors(error)));
+};
+
+export const receiveCurrentUser = user => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  };
+};
+
+export const receiveErrors = (error) => {
+  return {
+    type: RECEIVE_ERRORS,
+    error
+  };
+};
